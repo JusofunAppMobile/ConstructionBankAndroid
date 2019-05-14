f15d343e648461f21d291fa2e392861e151c983b
zhaoyapeng <515376293@qq.com>
2019-05-14 09:59:40 +0800

    no message

---
+++ app/src/main/java/com/qxb/jianhang/net/event/UpdateListEvent.java
@@ -0,0 +1,12 @@
+package com.qxb.jianhang.net.event;
+
+import com.jusfoun.baselibrary.event.IEvent;
+
+/**
+ * @author zhaoyapeng
+ * @version create time:2018/12/2815:50
+ * @Email zyp@jusfoun.com
+ * @Description ${更新企业列表}
+ */
+public class UpdateListEvent implements IEvent {
+}
